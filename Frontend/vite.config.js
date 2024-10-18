d6092fbc3d833ca4525d05388ceb49b491b7c0c7
abhishek sharma <abhisheksharma2920@gmail.com>
2024-10-18 21:30:19 +0530

    minor fix

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv';
-
-// Load environment variables from .env file
-dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_BE_URL,
+        target: 'https://sharemoments-project.onrender.com',
         changeOrigin: true,
         secure: false,
       },
